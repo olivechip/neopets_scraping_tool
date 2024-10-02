5172712d6fc7e59bfd561d729ca9bb733c122334
Oliver <oliver@MSI>
2024-10-01 18:45:39 -0700

    removed unnecessary headers

--- pet_img_scraper.py
+++ pet_img_scraper.py
@@ -2,16 +2,20 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-moods = ["Happy", "Sad", "Angry", "Sick"]
+moods = ["Happy", 
+# "Sad", "Angry", "Sick"
+]
 genders = ["male", "female"]
-colors = ["Red", "Blue", "Green", "Yellow"]
+colors = ["Red", 
+# "Blue", "Green", "Yellow"
+]
 
 # Commented out species are currently inactive on Neopets
 species = [
     "Acara", "Aisha", "Blumaroo", "Bori", "Bruce", "Buzz", "Chia", 
     # "Chomby", "Cybunny", "Draik", 
     "Elephante", "Eyrie", "Flotsam", "Gelert", "Gnorbu", "Grarrl", 
-    #"Grundo", "Hissi", 
+    # "Grundo", "Hissi", 
     "Ixi", 
     # "Jetsam", 
     "Jubjub", "Kacheek", "Kau", 
@@ -39,21 +43,8 @@ if not os.path.exists("urls"):
 
 # Construct headers with cookies
 headers = {
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "accept-encoding": "gzip, deflate, br, zstd",
     "accept-language": "en-US,en;q=0.9",
-    "cache-control": "no-cache",
-    "pragma": "no-cache",
-    "priority": "u=0, i",
     "referer": "https://www.sunnyneo.com/",
-    "sec-ch-ua": '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": '"Windows"',
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "cross-site",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"
 }
 
